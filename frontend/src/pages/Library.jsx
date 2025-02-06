c01e852928d77a6b95f23153a204a3562e4024bc
Antediluvianist <alexrob260@gmail.com>
2025-02-06 14:33:51 +0100

    Ajout de la fonctionalitée 'Trier par:' sur la page Library

--- frontend/src/pages/Library.jsx
+++ frontend/src/pages/Library.jsx
@@ -7,6 +7,7 @@ function Library() {
   const [books, setBooks] = useState([]);
   const [searchQuery, setSearchQuery] = useState("");
   const [selectedCategory, setSelectedCategory] = useState("Toutes");
+  const [sortOption, setSortOption] = useState("date_recent");
 
   const updateCover = async (book) => {
     try {
@@ -43,7 +44,7 @@ function Library() {
     const fetchBooks = async () => {
       const booksData = await getBooks();
       setBooks(booksData);
-
+      //console.log(books);
       booksData.forEach((book) => {
         if (!book.cover && book.coverUrl) {
           updateCover(book);
@@ -58,9 +59,36 @@ function Library() {
     setBooks(books.filter((book) => book.id !== bookId));
   };
 
+  const handleSortChange = (event) => {
+    setSortOption(event.target.value);
+  };
+
+  const sortedBooks = [...books].sort((a, b) => {
+    switch (sortOption) {
+      case "date_recent":
+        return b.yearRead - a.yearRead;
+      case "date_old":
+        return a.yearRead - b.yearRead;
+      case "title_asc":
+        return a.title.localeCompare(b.title);
+      case "title_desc":
+        return b.title.localeCompare(a.title);
+      case "author_asc":
+        return a.author.localeCompare(b.author);
+      case "author_desc":
+        return b.author.localeCompare(a.author);
+      case "rating_asc":
+        return a.rating - b.rating;
+      case "rating_desc":
+        return b.rating - a.rating;
+      default:
+        return 0;
+    }
+  });
+
   const filteredBooks = selectedCategory === "Toutes"
-    ? books
-    : books.filter((book) => book.genre1 === selectedCategory || book.genre2 === selectedCategory);
+    ? sortedBooks
+    : sortedBooks.filter((book) => book.genre1 === selectedCategory || book.genre2 === selectedCategory);
 
   return (
     <div style={styles.libraryContainer}>
@@ -68,6 +96,16 @@ function Library() {
       <div style={styles.libraryContent}>
         <h1>Ma Bibliothèque</h1>
         <div style={styles.inputContainer}>
+          <select id="sort-select" value={sortOption} onChange={handleSortChange} style={styles.select}>
+            <option value="date_recent">Trier par: Date de lecture (plus récent)</option>
+            <option value="date_old">Trier par: Date de lecture (plus ancien)</option>
+            <option value="title_asc">Trier par: Ordre alphabétique (A-Z)</option>
+            <option value="title_desc">Trier par: Ordre alphabétique (Z-A)</option>
+            <option value="author_asc">Trier par: Par auteur (A-Z)</option>
+            <option value="author_desc">Trier par: Par auteur (Z-A)</option>
+            <option value="rating_asc">Trier par: Par note (croissant)</option>
+            <option value="rating_desc">Trier par: Par note (décroissant)</option>
+          </select>
           <input
             type="text"
             placeholder="Rechercher un livre..."
@@ -79,9 +117,6 @@ function Library() {
         </div>
         <div style={styles.grid}>
           {filteredBooks
-            .filter((book) =>
-              book.title.toLowerCase().includes(searchQuery.toLowerCase())
-            )
             .map((book) => (
               <BookCard key={book.id} book={book} onDelete={handleDeleteBook} />
             ))}
@@ -108,6 +143,14 @@ const styles = {
     marginBottom: "20px",
     alignItems: "center",
   },
+  select: {
+    padding: "10px",
+    borderRadius: "5px",
+    border: "1px solid #ccc",
+    backgroundColor: "#1e1e1e",
+    color: "white",
+    outline: "none",
+  },
   input: {
     flex: 1,
     padding: "12px",
@@ -133,5 +176,6 @@ const styles = {
     padding: "10px",
   },
 };
+
 
 export default Library;
